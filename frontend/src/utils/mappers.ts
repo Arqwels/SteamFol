81466c5099e4880fed6aa39707b542c9625e069e
Arqwels <danil.kuzmin.1994@gmail.com>
2025-04-19 15:47:30 +0400

    добавить функцию преобразования Investment в данные таблицы

---
+++ frontend/src/utils/mappers.ts
@@ -0,0 +1,25 @@
+import { Investment } from '../types/investment';
+import { TableData } from '../types/tableData';
+
+/**
+ * Функция преобразует объект Investment (структура ответа API)
+ * в объект TableData, пригодный для отображения в таблице.
+ *
+ * Здесь можно добавить вычисления (например, процентов или прибыли за 24 часа),
+ * если соответствующие данные появятся.
+ */
+export function mapInvestmentToTableData(investment: Investment): TableData {
+  return {
+    id: investment.id,
+    market_name: investment.skin.market_name,
+    market_hash_name: investment.skin.market_hash_name,
+    price_item: investment.skin.price_skin,
+    //! Нужно будет сделать
+    change_price_percent_24h: 0,
+    change_price_profit_24h: 0,
+    image_url: investment.skin.image_url,
+    count_items: investment.countItems,
+    buy_price: investment.buyPrice,
+    currencyCode: investment.skin.currency_code,
+  };
+}
