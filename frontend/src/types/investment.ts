855ad96c090fc42ad4d00eac744ae87369c50623
Arqwels <danil.kuzmin.1994@gmail.com>
2025-04-19 15:53:11 +0400

    создать типы и мок-данные для инвестиций

---
+++ frontend/src/types/investment.ts
@@ -0,0 +1,18 @@
+import { Skin } from './skin';
+
+export interface Investment {
+  id: number;
+  idItem: number;
+  countItems: number;
+  buyPrice: number;
+  dateBuyItem: string;
+  updatedAt: string;
+  skin: Skin;
+}
+
+export interface CreateInvestmentRequest {
+  idItem: number;
+  countItems: number;
+  buyPrice: number;
+  dateBuyItem: string;
+}
---
+++ frontend/src/types/skin.ts
@@ -0,0 +1,9 @@
+export interface Skin {
+  id: number;
+  market_name: string;
+  market_hash_name: string;
+  price_skin: number;
+  image_url: string;
+  currency_code: string;
+  date_update: string;
+}
