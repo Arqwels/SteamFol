8ac8249148e3543de5facf0e3a2545355622ed76
Arqwels <danil.kuzmin.1994@gmail.com>
2025-04-20 16:23:45 +0400

    управлять цветом PlusIcon через CSS

--- frontend/src/components/Svg/PlusIcon.tsx
+++ frontend/src/components/Svg/PlusIcon.tsx
@@ -9,8 +9,8 @@ export default function PlusIcon () {
     >
       <path
         d="M14.1431 15.084L11.3147 12.2556L8.48627 15.084L7.54346 14.1412L10.3719 11.3128L7.54346 8.48437L8.48627 7.54156L11.3147 10.37L14.1431 7.54156L15.0859 8.48437L12.2575 11.3128L15.0859 14.1412L14.1431 15.084Z"
-        fill="white"
-        fillOpacity="0.2"
+        fill="currentColor"
+        fillOpacity="1"
       />
     </svg>
   );
