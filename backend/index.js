a9f035eacbdba8a987ad2ba4c4c5c8491353a333
Arqwels <danil.kuzmin.1994@gmail.com>
2025-05-13 15:36:50 +0400

    добавить поддержку метода PATCH в CORS

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 2000;
 app.use(express.json());
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use('/api', apiRouter);
