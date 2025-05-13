33b58544578293b5510180e04ec41c0f57ce57f5
Arqwels <danil.kuzmin.1994@gmail.com>
2025-05-13 15:31:15 +0400

    добавить эндпойнты переименования и удаления

--- backend/api/routes/portfolioRouter.js
+++ backend/api/routes/portfolioRouter.js
@@ -18,4 +18,12 @@ router.get('/active', portfolioController.getActivePortfolio);
 // PATCH http://localhost:5000/api/portfolio/:id/activate
 router.patch('/:id/activate', portfolioController.activatePortfolio);
 
+// Переименовать портфолио
+// PATCH http://localhost:5000/api/portfolio/:id
+router.patch('/:id', portfolioController.renamePortfolio);
+
+// Удаление портфолио
+// DELETE http://localhost:5000/api/portfolio/:id
+router.delete('/:id', portfolioController.deletePortfolio);
+
 module.exports = router;
