529b40357f4f69b55aae82559819d1fcd3aa00d0
Arqwels <danil.kuzmin.1994@gmail.com>
2025-05-17 22:40:17 +0400

    подключить activePortfolioReducer и типизировать store

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { baseApi } from '../api/baseApi';
+import activePortfolioReducer from './reducers/activePortfolioSlice';
 
 export const store = configureStore({
   reducer: {
-    [baseApi.reducerPath]: baseApi.reducer
+    [baseApi.reducerPath]: baseApi.reducer,
+    activePortfolio: activePortfolioReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
-})
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
