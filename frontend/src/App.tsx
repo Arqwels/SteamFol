793c36e680b7b5d41b788f99e332ed7413cd2679
Arqwels <danil.kuzmin.1994@gmail.com>
2025-06-04 23:04:26 +0400

    добавить логику обновления токена, роутинг и загрузчик в App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,47 @@
-import { MainPage } from './pages/MainPage';
+import { useEffect } from 'react';
+import { AppRoutes } from './routes/AppRoutes';
+import { useAppDispatch } from './stores/hooks';
+import { useRefreshQuery } from './api/authApi';
+import { logout, setCredentials } from './stores/reducers/authSlice';
+import { Loader } from './components/Loader/Loader';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  const { data, isLoading, isError, isUninitialized } = useRefreshQuery(
+    undefined,
+    {
+      refetchOnMountOrArgChange: false, // false — чтобы не дергать повторно
+    },
+  );
+
+  useEffect(() => {
+    if (isUninitialized) return;
+
+    if (data) {
+      dispatch(
+        setCredentials({
+          accessToken: data.accessToken,
+          refreshToken: data.refreshToken,
+          user: data.user,
+        }),
+      );
+    } else if (isError) {
+      dispatch(logout());
+    }
+  }, [isUninitialized, data, isError, dispatch]);
+
+  if (isUninitialized || isLoading) {
+    return <Loader />;
+  }
+
   return (
     <>
-      <MainPage />
+      <AppRoutes />
+      <Toaster position='top-center' />
     </>
-  )
+  );
 }
 
 export default App;
