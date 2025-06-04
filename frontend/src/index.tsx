b4067f58775873737b66e24411678bccf3f79582
Arqwels <danil.kuzmin.1994@gmail.com>
2025-06-04 14:18:57 +0400

    добавить BrowserRouter в точку входа

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './index.scss';
 import { Provider } from 'react-redux';
 import { store } from './stores/store.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );
